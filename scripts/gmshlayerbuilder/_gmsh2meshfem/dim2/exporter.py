bfe1304f76c02c8c8d872d314afff98679472696
Kentaro Hanson <kh3372@princeton.edu>
2025-10-10 19:42:23 -0400

    minor bugfixes from dg2d-kernelimpl

--- scripts/gmshlayerbuilder/_gmsh2meshfem/dim2/exporter.py
+++ scripts/gmshlayerbuilder/_gmsh2meshfem/dim2/exporter.py
@@ -162,6 +162,8 @@ class Exporter:
             nnodes = nodes_arr.shape[0]
             f.write(str(nnodes) + "\n")
 
+            assert nodes_arr.shape[1] == 2, "2d exporter received 3d points!"
+
             for inod in range(nnodes):
                 f.write(f"{nodes_arr[inod, 0]:.10f} {nodes_arr[inod, 1]:.10f}\n")
 
@@ -181,11 +183,7 @@ class Exporter:
         # materials
         # =========================
         with (self.destination_folder / self.materials_file).open("w") as f:
-
-            # no header entry
-
-            for mat in self.model.materials:
-                f.write(f"{mat}\n")
+            f.write("\n".join(str(mat) for mat in self.model.materials))
 
         # =========================
         # free surface
